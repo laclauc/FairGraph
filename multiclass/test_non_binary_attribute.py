54d6268630f79edf63565740971d200137feba8a
ievred <ievgen.redko@univ-st-etienne.fr>
2020-09-28 18:17:44 +0200

    correct mutliclass

--- multiclass/test_non_binary_attribute.py
+++ multiclass/test_non_binary_attribute.py
@@ -43,13 +43,13 @@ lambdast = np.ones((3,)) / 3
 
 #X_bary, couplings = ot.gromov.gromov_barycenters(n, Cs, ps=weights, p=b, lambdas= lambdast, loss_fun = 'square_loss', max_iter=100, tol=1e-3)
 
-X_bary, log = ot.lp.free_support_barycenter(measures_locations=Xs, measures_weights = weights, X_init = X_init, b = b, log=True, metric = 'sqeuclidean')
+X_bary, log = ot.lp.free_support_barycenter(measures_locations=Xs, measures_weights = weights, X_init = X_init, b = b, log=True)
 couplings = log['T']
 X0.append(X_bary)
 
 
 for i in range(len(classes)):
-    X_repaired.append(np.dot(couplings[i], X_bary))
+    X_repaired.append(np.dot(couplings[i].T, X_bary))
 
 X0.append(np.concatenate(X_repaired))
 
