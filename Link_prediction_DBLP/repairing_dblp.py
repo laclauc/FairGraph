baab78a07b89812ae96c7a0d0aff00db7ad927a8
laclauc <charlotte.laclau@gmail.com>
2020-09-28 18:52:41 +0200

    update multiclass

--- Link_prediction_DBLP/repairing_dblp.py
+++ Link_prediction_DBLP/repairing_dblp.py
@@ -30,7 +30,7 @@ for key, value in protS.items():
         new.pop(key)
 
 protS = new.copy()
-protS_array = protS.values()
+protS_array = np.array(list(protS.values()))
 
 X0 = []
 
@@ -38,7 +38,7 @@ X = nx.to_numpy_array(g)
 X0.append(X)
 n, d = X.shape
 
-classes = np.unique(protS)
+classes = np.unique(protS_array)
 
 idxs = []
 Xs = []
@@ -48,7 +48,7 @@ n_i = []
 Cs = []
 
 for i in classes:
-    idxs.append(np.concatenate(np.where(protS == i)))
+    idxs.append(np.concatenate(np.where(protS_array == i)))
     n_i.append(len(idxs[-1]))
     b_i = np.random.uniform(0., 1., (n_i[-1],))
     b_i = b_i / np.sum(b_i)  # Dirac weights
@@ -59,8 +59,7 @@ for i in classes:
 
 X_init = np.random.normal(0., 1., X.shape)  # initial Dirac locations
 b = np.ones((n,)) / n  # weights of the barycenter (it will not be optimized, only the locations are optimized)
-lambdast = np.ones((3,)) / 3
-
+# lambdast = np.ones((3,)) / 3
 
 X_bary, log = ot.lp.free_support_barycenter(measures_locations=Xs, measures_weights=weights, X_init=X_init, b=b,
                                             log=True, metric='sqeuclidean')
@@ -68,10 +67,11 @@ couplings = log['T']
 X0.append(X_bary)
 
 for i in range(len(classes)):
-    X_repaired.append(np.dot(couplings[i], X_bary))
+    X_repaired.append(np.dot(couplings[i].T, X_bary))
 
 X0.append(np.concatenate(X_repaired))
 new_adj = np.concatenate(X_repaired)
+
 new_g = nx.from_numpy_matrix(new_adj)
 
 repaired_graph = [new_g, protS]
